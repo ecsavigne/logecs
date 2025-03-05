03dc732b6230142439c0f46bac2c128256e4e812
ecsavigne <ecsavigne@gmail.com>
2025-03-05 11:37:58 -0300

    Add flags for register line of the error.
    Fix logger equal nil when path is nil o output false
    
    Add propierty 'NotStandardPut' for controller output standard

--- log/wrapper.go
+++ log/wrapper.go
@@ -38,13 +38,14 @@ type Logger interface {
 }
 
 type EcsLogger struct {
-	Mod      string // Name of module of the log, exemple: "NameService"
-	Color    bool   // If true, then info, warn and error logs will be colored cyan, yellow and red respectively using ANSI color escape codes
-	OutPut   bool   // If true, then the log will be output to stdout
-	min      int
-	LevelMin string
-	logger   *log.Logger
-	Path     string // path to the log file
+	Mod            string // Name of module of the log, exemple: "NameService"
+	Color          bool   // If true, then info, warn and error logs will be colored cyan, yellow and red respectively using ANSI color escape codes
+	OutPut         bool   // If true, then the log will be output to stdout
+	min            int
+	NotStandardPut bool // If true, then the log will be to standard output. If not especified do not output to standard output
+	LevelMin       string
+	logger         *log.Logger
+	Path           string // path to the log file
 }
 
 var colors = map[string]string{
@@ -75,12 +76,16 @@ func (s *EcsLogger) outputf(level, msg string, args ...interface{}) {
 		if s.logger != nil {
 			s.logger.Printf("%s%s [%s] %s%s\n", time.Now().Format("15:04:05.000"), colorStart, level, fmt.Sprintf(msg, args...), colorReset)
 		}
-		fmt.Printf("%s%s [%s] %s%s\n", time.Now().Format("15:04:05.000"), colorStart, level, fmt.Sprintf(msg, args...), colorReset)
+		if !s.NotStandardPut {
+			fmt.Printf("%s%s [%s] %s%s\n", time.Now().Format("15:04:05.000"), colorStart, level, fmt.Sprintf(msg, args...), colorReset)
+		}
 	} else {
 		if s.logger != nil {
 			s.logger.Printf("%s%s [%s %s] %s%s\n", time.Now().Format("15:04:05.000"), colorStart, s.Mod, level, fmt.Sprintf(msg, args...), colorReset)
 		}
-		fmt.Printf("%s%s [%s %s] %s%s\n", time.Now().Format("15:04:05.000"), colorStart, s.Mod, level, fmt.Sprintf(msg, args...), colorReset)
+		if !s.NotStandardPut {
+			fmt.Printf("%s%s [%s %s] %s%s\n", time.Now().Format("15:04:05.000"), colorStart, s.Mod, level, fmt.Sprintf(msg, args...), colorReset)
+		}
 	}
 }
 
@@ -130,7 +135,7 @@ func (s *EcsLogger) Debugf(msg string, args ...interface{}) { s.outputf("DEBUG",
 
 // func (s *EcsLogger) Sub(mod string) Logger
 func (s *EcsLogger) Sub(mod string) Logger {
-	return &EcsLogger{Mod: fmt.Sprintf("%s/%s", s.Mod, mod), Color: s.Color, min: s.min}
+	return &EcsLogger{Mod: fmt.Sprintf("%s/%s", s.Mod, mod), Color: s.Color, min: s.min, logger: s.logger, Path: s.Path, OutPut: s.OutPut}
 }
 
 // Stdout is a simple Logger implementation that outputs to stdout. The module name given is included in log lines.
@@ -142,20 +147,23 @@ func stdout(module string, minLevel, path string, color, output bool) Logger {
 	var (
 		fileLog *os.File
 		err     error
+		logger  *log.Logger
 	)
 	if path != "" && output {
 		fileLog, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
 		if err != nil {
 			panic(fmt.Sprintln("Error opening file:", err))
 		}
+		logger = log.New(fileLog, "", log.Ldate|log.Ltime|log.Lshortfile|log.Llongfile)
 	}
+
 	return &EcsLogger{
 		Mod:    module,
 		Color:  color,
 		OutPut: output,
 		Path:   path,
 		min:    levelToInt[strings.ToUpper(minLevel)],
-		logger: log.New(fileLog, "", log.Ldate|log.Ltime|log.Lshortfile),
+		logger: logger,
 	}
 }
 
